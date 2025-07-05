de5dbbfb7a016f509f544b643a2e08067c10284b
prot-alx <prot.alx.92@gmail.com>
2025-07-06 00:36:49 +0500

    url fix

--- apps/web/config/config.ts
+++ apps/web/config/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.PROD ? "/api" : "http://localhost:3001";
+export const API_URL = import.meta.env.PROD ? "http://editor.ddns.net:49236" : "http://localhost:3001";
